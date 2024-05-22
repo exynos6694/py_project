99004be16b7c8ff5d0a4f80c579d446be2602f34
exynos6694 <azsx6694@naver.com>
2024-05-22 12:39:56 +0900

    #1

--- main.py
+++ main.py
@@ -1,11 +1,23 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
+from fastapi.responses import HTMLResponse
+from fastapi.templating import Jinja2Templates
+from fastapi.staticfiles import StaticFiles
 
 app = FastAPI()
 
-@app.get("/")
-def read_root():
-    return {"Hello": "World"}
+# Jinja2 템플릿 디렉토리를 설정합니다.
+templates = Jinja2Templates(directory="templates")
 
+# 정적 파일을 제공하기 위한 디렉토리를 설정합니다.
+app.mount("/static", StaticFiles(directory="static"), name="static")
+
+@app.get("/", response_class=HTMLResponse)
+async def read_root(request: Request):
+    # HTML 템플릿에 데이터를 전달하여 렌더링합니다.
+    return templates.TemplateResponse("base.html", {"request": request, "name": "World"})
+
+
+# uvicorn main:app --reload 명령어로 실행
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="127.0.0.1", port=8000)
+    uvicorn.run(app, host="182.214.82.181", port=8080)
