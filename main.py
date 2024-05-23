70ec176ae3ee7801a9df8f2523735f0642390e2b
exynos6694 <azsx6694@naver.com>
2024-05-23 10:52:41 +0900

    #3

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from fastapi import FastAPI, Request
 from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
 from fastapi.staticfiles import StaticFiles
+from pymongo import MongoClient
 
 app = FastAPI()
 
